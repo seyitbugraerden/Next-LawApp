10ba094ac9ad5c121e82ed7abcab6efb0f0bc699
seyitbugraerden <seyitbugraerden@gmail.com>
2024-12-28 02:20:56 +0300

    about us section responsive

--- components/home/AboutUs.tsx
+++ components/home/AboutUs.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export const AboutUs = () => {
   return (
-    <section>
+    <section className="py-24 lg:py-0">
       <Container>
         <div className="flex flex-row justify-between items-end">
           <div className="flex flex-col gap-2 border-l-2 border-secondary ps-4">
@@ -11,24 +11,24 @@ export const AboutUs = () => {
             <h1 className="text-4xl text-white">About Us</h1>
           </div>
         </div>
-        <div className="flex items-start gap-36 text-white mt-12">
-          <div className="flex-1 relative">
+        <div className="flex flex-col lg:flex-row items-start gap-24 lg:gap-36 text-white mt-12">
+          <div className="flex-1 relative order-2 lg:order-1">
             <Image
               src="https://picsum.photos/640/640"
               alt="left pic"
               width={640}
               height={120}
-              className="aspect-square w-[80%] !h-[70dvh]"
+              className="lg:aspect-square lg:w-[80%] lg:!h-[70dvh] w-screen object-cover"
             />
             <Image
               src="https://picsum.photos/640/640"
               alt="left pic"
               width={640}
               height={120}
-              className="aspect-square absolute right-0 top-1/2 -translate-y-1/2 w-1/2 shadow-2xl"
+              className="aspect-square absolute right-0 top-1/2 -translate-y-1/2 w-1/2 shadow-2xl hidden lg:block"
             />
           </div>
-          <div className="flex-1 flex flex-col gap-6">
+          <div className="flex-1 flex flex-col gap-6 order-1 lg:order-2">
             <h2 className="text-3xl font-bold">
               We Are The Most Populer Law Firm That Can Help You Perfectly!
             </h2>
