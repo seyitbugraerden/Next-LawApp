d2b3cc6c7963d6cb5d6b2c4cd31ebb82fa92c8ef
seyitbugraerden <seyitbugraerden@gmail.com>
2024-12-28 23:35:39 +0300

    page title fixes

--- components/pages/PagesTitle.tsx
+++ components/pages/PagesTitle.tsx
@@ -9,9 +9,9 @@ export const PagesTitle = ({
 }) => {
   return (
     <div className="flex justify-center items-center flex-col gap-2 my-12 ">
-      <h1 className="text-xl text-secondary">{title}</h1>{" "}
-      <h3 className="text-4xl text-white text-center">{subTitle}</h3>
-      <div className="w-14 h-[1px] bg-secondary mt-6"></div>
+      <h1 className="text-sm lg:text-xl text-secondary">{title}</h1>{" "}
+      <h3 className="text-lg lg:text-4xl text-white text-center">{subTitle}</h3>
+      <div className="w-14 h-[1px] bg-secondary mt-3 lg:mt-6"></div>
     </div>
   );
 };
