547f18a277cfea8d007f41ea5fcfdcd3a91b758b
seyitbugraerden <seyitbugraerden@gmail.com>
2024-12-29 00:02:05 +0300

    Hizmetler responsive fixes

--- app/(pages)/hizmetlerimiz/[slug]/page.tsx
+++ app/(pages)/hizmetlerimiz/[slug]/page.tsx
@@ -18,8 +18,8 @@ const page = () => {
       "
       />
       <Container className="text-white my-12">
-        <div className="relative overflow-hidden rounded-2xl shadow-2xl shadow-black/70">
-          <p className="text-white p-12">
+        <div className="relative overflow-hidden rounded-2xl lg:shadow-2xl lg:shadow-black/70">
+          <p className="text-white lg:p-12">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
             Repellendus deleniti error veritatis at, debitis odio neque iusto?
             Corporis doloribus omnis quia error ullam aliquam id porro. Quaerat
@@ -38,46 +38,61 @@ const page = () => {
             alt=""
             width={1920}
             height={300}
-            className="absolute left-0 top-0 opacity-30 -z-50 pointer-events-none object-cover h-full"
+            className="absolute hidden lg:block left-0 top-0 opacity-30 -z-50 pointer-events-none object-cover h-full"
           />
         </div>
       </Container>
       <Container>
-        <div className="flex-1 flex flex-col gap-6 p-12 lg:p-24 shadow-2xl shadow-black/70 max-w-2xl mx-auto my-24">
-          <h2 className="text-white text-4xl bg-transparent">
-            Bilgi ve Randevu İçin
+        <div className="flex-1 flex flex-col gap-6 p-12 px-4 lg:p-24 shadow-2xl shadow-black/70 max-w-2xl mx-auto my-24">
+          <h2 className="text-white text-4xl bg-transparent text-center lg:text-start">
+            Bilgi ve Randevu
           </h2>
           <div className="w-full h-[1px] bg-primary"></div>
           <input
             type="text"
-            placeholder="Your Name*"
+            placeholder="Adınız"
             required
-            className="border border-white bg-transparent focus:outline-none focus:border-white p-3 placeholder:text-white text-white"
+            className="border border-white/50 bg-transparent focus:outline-none focus:border-white/50 p-3 placeholder:text-white text-white"
           />
           <input
             type="email"
-            placeholder="Your Email*"
+            placeholder="E-Posta Adresiniz"
             required
-            className="border border-white bg-transparent focus:outline-none focus:border-white p-3 placeholder:text-white text-white"
+            className="border border-white/50 bg-transparent focus:outline-none focus:border-white/50 p-3 placeholder:text-white text-white"
           />
           <input
             type="tel"
-            placeholder="Phone*"
+            placeholder="Telefon Numaranız"
             required
-            className="border border-white bg-transparent focus:outline-none focus:border-white p-3 placeholder:text-white text-white"
+            className="border border-white/50 bg-transparent focus:outline-none focus:border-white/50 p-3 placeholder:text-white text-white"
           />
-          <select className="border border-white bg-transparent focus:outline-none focus:border-white p-3 text-white">
-            <option value="Subject*">Subject*</option>
+          <select className="border border-white/50 bg-transparent focus:outline-none focus:border-white/50 p-3 text-white">
+            <option className="bg-primary" value="Konu*">Konu</option>
+            <option className="bg-primary" value="Şirketler Hukuku Danışmanlığı">
+              Şirketler Hukuku Danışmanlığı
+            </option>
+            <option className="bg-primary" value="Sözleşme Hukuku Danışmanlığı*">
+              Sözleşme Hukuku Danışmanlığı
+            </option>
+            <option className="bg-primary" value="Fikri ve Sınai Haklar Hukuku*">
+              Fikri ve Sınai Haklar Hukuku
+            </option>
+            <option className="bg-primary" value="Gayrimenkul Hukuku Danışmanlığı">
+              Gayrimenkul Hukuku Danışmanlığı
+            </option>
+            <option className="bg-primary" value="İş ve Sosyal Güvenlik Hukuku">
+              İş ve Sosyal Güvenlik Hukuku
+            </option>
           </select>
           <textarea
             rows={5}
-            placeholder="Case Description"
-            className="border border-white bg-transparent focus:outline-none focus:border-white p-3 placeholder:text-white text-white"
+            placeholder="Konu Açıklaması"
+            className="border border-white/50 bg-transparent focus:outline-none focus:border-white/50 p-3 placeholder:text-white text-white"
           />
           <div className="bg-transparent flex justify-center mt-auto">
             <Link
               href="/"
-              className="px-6 py-2 border border-white bg-transparent text-white"
+              className="px-6 py-2 border border-white/50 bg-transparent text-white"
             >
               Randevu Al
             </Link>
