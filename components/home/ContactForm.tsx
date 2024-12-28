cd2cc1b03299f33cc501c9993014f14abce0784e
seyitbugraerden <seyitbugraerden@gmail.com>
2024-12-29 00:19:47 +0300

    contact form

--- components/home/ContactForm.tsx
+++ components/home/ContactForm.tsx
@@ -6,12 +6,12 @@ import Link from "next/link";
 
 export const ContactForm = () => {
   return (
-    <section className="py-12 lg:py-24 bg-tertiary overflow-y-hidden">
+    <section className="mt-12 py-6 lg:py-24 bg-tertiary overflow-y-hidden">
       <Container className="flex flex-col lg:flex-row lg:px-24 bg-transparent">
-        <div className="flex-1 flex flex-col gap-6 p-12 lg:p-24">
-          <h2 className="text-white text-4xl bg-transparent">Contact Info</h2>
+        <div className="flex-1 flex flex-col gap-6 p-4 pb-12 lg:p-24">
+          <h2 className="text-white text-xl lg:text-4xl bg-transparent">Contact Info</h2>
           <div className="w-full h-[1px] bg-secondary"></div>
-          <p className="text-white text-lg bg-transparent">
+          <p className="text-white text-sm lg:text-lg bg-transparent">
             Amet turpis urna malesuada sed augue ut. Orci neque elit massa
             semper turpis vel. Enim tortor at hac quis odio. Quam nulla gravida
             sit gravida vitae fermentum parturient.
@@ -19,70 +19,85 @@ export const ContactForm = () => {
           <div className="flex flex-col items-start gap-6">
             <div className="flex flex-row items-center gap-4">
               <div>
-                <IoHomeOutline className="text-white size-12" />
+                <IoHomeOutline className="text-white size-8 lg:size-12" />
               </div>
               <div className="text-white flex flex-col items-start gap-2">
-                <h2 className="text-2xl font-semibold">Office Address</h2>
-                <p className="text-sm">244 Royal Ln. Mesa, New Jersey</p>
+                <h2 className="font-semibold text-sm lg:text-2xl">Office Address</h2>
+                <p className="text-xs lg:text-sm">244 Royal Ln. Mesa, New Jersey</p>
               </div>
             </div>
             <div className="flex flex-row items-center gap-4">
               <div>
-                <MdOutlineAttachEmail className="text-white size-12" />
+                <MdOutlineAttachEmail className="text-white size-8 lg:size-12" />
               </div>
               <div className="text-white flex flex-col items-start gap-2">
-                <h2 className="text-2xl font-semibold">Email</h2>
-                <p className="text-sm">justuprodemo@gmail.com</p>
+                <h2 className="font-semibold text-sm lg:text-2xl">Email</h2>
+                <p className="text-xs lg:text-sm">justuprodemo@gmail.com</p>
               </div>
             </div>
             <div className="flex flex-row items-center gap-4">
               <div>
-                <MdOutlineLocalPhone className="text-white size-12" />
+                <MdOutlineLocalPhone className="text-white size-8 lg:size-12" />
               </div>
               <div className="text-white flex flex-col items-start gap-2">
-                <h2 className="text-2xl font-semibold">Phone</h2>
-                <p className="text-sm">(684) 555-0102</p>
+                <h2 className="text-sm lg:text-2xl font-semibold">Phone</h2>
+                <p className="text-xs lg:text-sm">(684) 555-0102</p>
               </div>
             </div>
             <div className="flex flex-row items-center gap-4">
               <div>
-                <TbClockHour4 className="text-white size-12" />
+                <TbClockHour4 className="text-white size-8 lg:size-12" />
               </div>
               <div className="text-white flex flex-col items-start gap-2">
-                <h2 className="text-2xl font-semibold">Office Hour</h2>
-                <p className="text-sm">Mon-Sun: 10am – 7pm</p>
+                <h2 className="text-sm lg:text-2xl font-semibold">Office Hour</h2>
+                <p className="text-xs lg:text-sm">Mon-Sun: 10am – 7pm</p>
               </div>
             </div>
           </div>
         </div>
-        <div className="flex-1 flex flex-col gap-6 p-12 lg:p-24 bg-secondary shadow-2xl shadow-black/70">
-          <h2 className="text-white text-4xl bg-transparent">Contact Us</h2>
+        <div className="flex-1 flex flex-col gap-6 p-4 lg:p-24 bg-secondary shadow-2xl shadow-black/70">
+          <h2 className="text-white text-xl lg:text-4xl bg-transparent">Contact Us</h2>
           <div className="w-full h-[1px] bg-primary"></div>
           <input
             type="text"
-            placeholder="Your Name*"
+            placeholder="Adınız"
             required
-            className="border border-white bg-transparent focus:outline-none focus:border-white p-3 placeholder:text-white text-white"
+            className="border border-white bg-transparent text-xs lg:text-sm focus:outline-none focus:border-white p-3 placeholder:text-white text-white"
           />
           <input
             type="email"
-            placeholder="Your Email*"
+            placeholder="E-Posta Adresiniz"
             required
-            className="border border-white bg-transparent focus:outline-none focus:border-white p-3 placeholder:text-white text-white"
+            className="border border-white bg-transparent text-xs lg:text-sm focus:outline-none focus:border-white p-3 placeholder:text-white text-white"
           />
           <input
             type="tel"
-            placeholder="Phone*"
+            placeholder="Telefon Numaranız"
             required
-            className="border border-white bg-transparent focus:outline-none focus:border-white p-3 placeholder:text-white text-white"
+            className="border border-white bg-transparent text-xs lg:text-sm focus:outline-none focus:border-white p-3 placeholder:text-white text-white"
           />
-          <select className="border border-white bg-transparent focus:outline-none focus:border-white p-3 text-white">
-            <option value="Subject*">Subject*</option>
+          <select className="border border-white bg-transparent text-xs lg:text-sm focus:outline-none focus:border-white p-3 text-white">
+          <option className="bg-primary" value="Konu*">Konu</option>
+            <option className="bg-primary" value="Şirketler Hukuku Danışmanlığı">
+              Şirketler Hukuku Danışmanlığı
+            </option>
+            <option className="bg-primary" value="Sözleşme Hukuku Danışmanlığı*">
+              Sözleşme Hukuku Danışmanlığı
+            </option>
+            <option className="bg-primary" value="Fikri ve Sınai Haklar Hukuku*">
+              Fikri ve Sınai Haklar Hukuku
+            </option>
+            <option className="bg-primary" value="Gayrimenkul Hukuku Danışmanlığı">
+              Gayrimenkul Hukuku Danışmanlığı
+            </option>
+            <option className="bg-primary" value="İş ve Sosyal Güvenlik Hukuku">
+              İş ve Sosyal Güvenlik Hukuku
+            </option>
           </select>
           <textarea
             rows={5}
-            placeholder="Case Description"
-            className="border border-white bg-transparent focus:outline-none focus:border-white p-3 placeholder:text-white text-white"
+            placeholder="Açıklama"
+            className="border border-white bg-transparent text-xs lg:text-sm focus:outline-none focus:border-white p-3 placeholder:text-white text-white"
           />
           <div className="bg-transparent flex justify-center mt-auto">
             <Link
