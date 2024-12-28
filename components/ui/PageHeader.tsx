25937f5fc6539f26ac16e43eae355e953a14a224
seyitbugraerden <seyitbugraerden@gmail.com>
2024-12-28 23:32:34 +0300

    pageheader fixes

--- components/ui/PageHeader.tsx
+++ components/ui/PageHeader.tsx
@@ -10,14 +10,14 @@ interface BreadcrumbProps {
 }
 export const PageHeader = ({ title, items = [] }: BreadcrumbProps) => {
   return (
-    <div className="w-full h-[400px] bg-black/30 pl-12 xl:pl-0">
+    <div className="w-full h-[200px] lg:h-[400px] bg-black/30 pl-12 xl:pl-0">
       <div className="max-w-7xl mx-auto h-full">
         <div className="flex flex-col justify-center items-start h-full">
-          <h1 className="text-2xl lg:text-6xl text-white">{title}</h1>
+          <h1 className="text-2xl lg:text-6xl text-white line-clamp-1">{title}</h1>
           <div className="flex flex-row items-center text-2xl gap-2 mt-3 text-secondary">
             <Link
               href="/"
-              className="hover:text-secondary/50 transition duration-300 text-sm lg:text-2xl"
+              className="hover:text-secondary/50 transition duration-300 text-sm lg:text-2xl whitespace-nowrap line-clamp-1"
             >
               Ana Sayfa
             </Link>
@@ -26,7 +26,7 @@ export const PageHeader = ({ title, items = [] }: BreadcrumbProps) => {
                 <MdOutlineKeyboardArrowRight className="mt-0.5 size-2 lg:size-8" />
                 <Link
                   href={item.link}
-                  className="hover:text-secondary/50 transition duration-300 text-sm lg:text-2xl"
+                  className="hover:text-secondary/50 transition duration-300 text-sm lg:text-2xl whitespace-nowrap line-clamp-1"
                 >
                   {item.title}
                 </Link>
