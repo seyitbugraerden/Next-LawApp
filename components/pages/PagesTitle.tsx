4e97ecb444569a66a11b34129e6be811ad2a58e9
seyitbugraerden <seyitbugraerden@gmail.com>
2024-12-28 23:38:04 +0300

    page title fix

--- components/pages/PagesTitle.tsx
+++ components/pages/PagesTitle.tsx
@@ -8,7 +8,7 @@ export const PagesTitle = ({
   subTitle: string;
 }) => {
   return (
-    <div className="flex justify-center items-center flex-col gap-2 my-12 ">
+    <div className="flex justify-center items-center flex-col gap-2 mt-8 mb-0 lg:my-12">
       <h1 className="text-sm lg:text-xl text-secondary">{title}</h1>{" "}
       <h3 className="text-lg lg:text-4xl text-white text-center">{subTitle}</h3>
       <div className="w-14 h-[1px] bg-secondary mt-3 lg:mt-6"></div>
