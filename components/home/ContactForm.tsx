9f68bda059f71226b45a661d31f2f228b96e3c17
seyitbugraerden <seyitbugraerden@gmail.com>
2024-12-29 00:29:33 +0300

    texts

--- components/home/ContactForm.tsx
+++ components/home/ContactForm.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export const ContactForm = () => {
   return (
-    <section className="mt-12 lg:py-6 lg:py-24 bg-tertiary overflow-y-hidden">
+    <section className="mt-12 lg:py-24 bg-tertiary overflow-y-hidden">
       <Container className="flex flex-col lg:flex-row lg:px-24 bg-transparent">
         <div className="flex-1 flex flex-col gap-6 p-4 pb-12 lg:p-24">
           <h2 className="text-white text-xl lg:text-4xl bg-transparent">Contact Info</h2>
@@ -62,21 +62,21 @@ export const ContactForm = () => {
             type="text"
             placeholder="Adınız"
             required
-            className="border border-white/30 bg-transparent text-xs lg:text-sm focus:outline-none focus:border-white/30 p-3 placeholder:text-white/30 text-white"
+            className="border border-primary/30 bg-transparent text-xs lg:text-sm focus:outline-none focus:border-primary/30 p-3 placeholder:text-primary/30 text-white"
           />
           <input
             type="email"
             placeholder="E-Posta Adresiniz"
             required
-            className="border border-white/30 bg-transparent text-xs lg:text-sm focus:outline-none focus:border-white/30 p-3 placeholder:text-white/30 text-white"
+            className="border border-primary/30 bg-transparent text-xs lg:text-sm focus:outline-none focus:border-primary/30 p-3 placeholder:text-primary/30 text-white"
           />
           <input
             type="tel"
             placeholder="Telefon Numaranız"
             required
-            className="border border-white/30 bg-transparent text-xs lg:text-sm focus:outline-none focus:border-white/30 p-3 placeholder:text-white/30 text-white"
+            className="border border-primary/30 bg-transparent text-xs lg:text-sm focus:outline-none focus:border-primary/30 p-3 placeholder:text-primary/30 text-white"
           />
-          <select className="border border-white/30 bg-transparent text-xs lg:text-sm focus:outline-none focus:border-white/30 p-3 text-white/30">
+          <select className="border border-primary/30 bg-transparent text-xs lg:text-sm focus:outline-none focus:border-primary/30 p-3 text-primary/30">
           <option className="bg-primary" value="Konu*">Konu</option>
             <option className="bg-primary" value="Şirketler Hukuku Danışmanlığı">
               Şirketler Hukuku Danışmanlığı
@@ -97,12 +97,12 @@ export const ContactForm = () => {
           <textarea
             rows={5}
             placeholder="Açıklama"
-            className="border border-white/30 bg-transparent text-xs lg:text-sm focus:outline-none focus:border-white/30 p-3 placeholder:text-white/30 text-white"
+            className="border border-primary/30 bg-transparent text-xs lg:text-sm focus:outline-none focus:border-primary/30 p-3 placeholder:text-primary/30 text-white"
           />
           <div className="bg-transparent flex justify-center mt-auto">
             <Link
               href="/"
-              className="px-6 py-2 border border-white/30 bg-transparent text-white"
+              className="px-6 py-2 border border-primary/30 bg-transparent text-white"
             >
               Randevu Al
             </Link>
