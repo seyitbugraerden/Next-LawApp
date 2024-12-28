851648e5aa1f3cccd11f3957546b13a982f2cb3a
seyitbugraerden <seyitbugraerden@gmail.com>
2024-12-28 23:31:50 +0300

    pageheader fix

--- components/ui/PageHeader.tsx
+++ components/ui/PageHeader.tsx
@@ -13,20 +13,20 @@ export const PageHeader = ({ title, items = [] }: BreadcrumbProps) => {
     <div className="w-full h-[400px] bg-black/30 pl-12 xl:pl-0">
       <div className="max-w-7xl mx-auto h-full">
         <div className="flex flex-col justify-center items-start h-full">
-          <h1 className="text-6xl text-white">{title}</h1>
+          <h1 className="text-2xl lg:text-6xl text-white">{title}</h1>
           <div className="flex flex-row items-center text-2xl gap-2 mt-3 text-secondary">
             <Link
               href="/"
-              className="hover:text-secondary/50 transition duration-300"
+              className="hover:text-secondary/50 transition duration-300 text-sm lg:text-2xl"
             >
               Ana Sayfa
             </Link>
             {items.map((item, index) => (
               <div key={index} className="flex items-center gap-2">
-                <MdOutlineKeyboardArrowRight className="mt-0.5" />
+                <MdOutlineKeyboardArrowRight className="mt-0.5 size-2 lg:size-8" />
                 <Link
                   href={item.link}
-                  className="hover:text-secondary/50 transition duration-300"
+                  className="hover:text-secondary/50 transition duration-300 text-sm lg:text-2xl"
                 >
                   {item.title}
                 </Link>
