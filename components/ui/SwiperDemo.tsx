b596d0a257ea0d335c155a184835bc90fa7b7103
seyitbugraerden <seyitbugraerden@gmail.com>
2024-12-30 02:28:55 +0300

    swiper section

--- components/ui/SwiperDemo.tsx
+++ components/ui/SwiperDemo.tsx
@@ -2,9 +2,11 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
+import NavData from "@/mock/navData.json";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -14,8 +16,7 @@ export default function SwiperDemo() {
     <>
       <Swiper
         slidesPerView={1}
-        loop={true}
-        className="mySwiper"
+        className="mySwiper __services h-[600px]"
         breakpoints={{
           640: {
             slidesPerView: 1,
@@ -27,8 +28,12 @@ export default function SwiperDemo() {
             slidesPerView: 3,
           },
         }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
       >
-        {Array.from({ length: 5 }).map((_, index) => (
+        {NavData[2].children.map((_, index) => (
           <SwiperSlide key={index} className="relative p-4">
             <Link href="#">
               <div className="relative">
@@ -40,11 +45,11 @@ export default function SwiperDemo() {
                   className="lg:h-[550px] w-full rounded-2xl brightness-50 hover:brightness-[20%] transition duration-300"
                 />
                 <div className="absolute right-6 bottom-6 z-50 text-secondary">
-                  Lorem ipsum dolor
+                  Hizmetlerimiz
                 </div>
                 <div className="absolute right-6 bottom-12 z-50">
                   <h2 className="text-3xl font-semibold text-white text-right">
-                    Hukuki Danışmanlık
+                    {_.title}
                   </h2>
                 </div>
               </div>
